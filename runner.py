b2e41d6525cb33321e5ee74945fbe77590c638fd
Liyi Zhang <36095057+zhang-liyi@users.noreply.github.com>
2020-12-31 18:09:11 +0800

    Minibatch training

--- runner.py
+++ runner.py
@@ -3,21 +3,24 @@ import numpy as np
 import argparse
 import pandas as pd
 
+# export KMP_DUPLICATE_LIB_OK=TRUE
+
 if __name__ == "__main__":
 
-    real_data_corona = False
+    corona_data = False
     real_data_1 = False
     real_data_2 = False
     load_strings = False
     simulate_data = False
-    primate_data = True
+    simulate_load_data = False
+    primate_data = False
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', default='primate_data')
-    parser.add_argument('--memory_optimization', default='off')
+    parser.add_argument('--dataset', default='load_strings')
+    parser.add_argument('--memory_optimization', default='on')
     args = parser.parse_args()
 
-    exec(args.dataset + '=True')
+    exec(args.dataset + ' = True')
 
     Alphabet_dir = {'A': [1, 0, 0, 0],
                     'C': [0, 1, 0, 0],
@@ -28,8 +31,6 @@ if __name__ == "__main__":
                     'g': [0, 0, 1, 0],
                     't': [0, 0, 0, 1]}
     alphabet = np.array([[1., 0., 0., 0.], [0., 1., 0., 0.], [0., 0., 1., 0.], [0., 0., 0., 1.]])
-
-
 
 
     def simulateDNA(nsamples, seqlength, alphabet):
@@ -57,16 +58,42 @@ if __name__ == "__main__":
         datadict = {'taxa': taxa,
                     'genome': data_NxSxA}
 
+    if simulate_load_data:
+        df = pd.read_csv('rdm_3.csv')
+        genome_strings = []
+        for i in range(1,4):
+            genome_strings.append(df.iat[0,1])
+        datadict = form_dataset_from_strings(genome_strings, alphabet_dir)
+
+
     if load_strings:
         genome_strings = ['ACTTTGAGAG', 'ACTTTGACAG', 'ACTTTGACTG', 'ACTTTGACTC']
         datadict = form_dataset_from_strings(genome_strings, Alphabet_dir)
 
-    if real_data_corona:
-        max_site = 300
-        datadict_raw = pd.read_pickle('betacoronavirus/betacoronavirus4.pickle')
+    if corona_data:
+        #max_site = 300
+        datadict_raw = pd.read_pickle('data/betacoronavirus4.pickle')
         datadict = {}
         datadict['taxa'] = datadict_raw['taxa']
-        datadict['genome'] = np.array(datadict_raw['genome'])[:, 0:max_site, :]
+        datadict['genome'] = np.array(datadict_raw['genome'])[:, 10000:10100, :]
+        # print(datadict['taxa'])
+        # for i in range(100):
+        #     print(datadict['genome'][0,i,:])
+
+    if primate_data:
+        Alphabet_dir_blank = {'A': [1, 0, 0, 0, 0],
+                              'C': [0, 1, 0, 0, 0],
+                              'G': [0, 0, 1, 0, 0],
+                              'T': [0, 0, 0, 1, 0],
+                              '-': [0, 0, 0, 0, 1]}
+
+        datadict_raw = pd.read_pickle('data/primate.p')
+        # max_site = 898
+        # datadict = {}
+        # datadict['taxa'] = datadict_raw.keys()
+        # datadict['genome'] = np.array(datadict_raw.values())
+        genome_strings = list(datadict_raw.values())
+        datadict = form_dataset_from_strings(genome_strings, Alphabet_dir_blank)
 
     if real_data_1:
         genome_strings = \
@@ -96,23 +123,8 @@ if __name__ == "__main__":
         datadict['taxa'] = ['human', 'chimp', 'gorilla', 'oranguta', 'gibbon', 'rhesus', 'macaque', 'baboon',
                             'greenmonkey']
 
-    if primate_data:
-        Alphabet_dir_blank = {'A': [1, 0, 0, 0, 0],
-                              'C': [0, 1, 0, 0, 0],
-                              'G': [0, 0, 1, 0, 0],
-                              'T': [0, 0, 0, 1, 0],
-                              '-': [0, 0, 0, 0, 1]}
-
-        #pdb.set_trace()
-        datadict_raw = pd.read_pickle('data/primate.p')
-        # max_site = 898
-        datadict = {}
-        datadict['taxa'] = datadict_raw.keys()
-        datadict['genome'] = np.array(datadict_raw.values())
-        genome_strings = list(datadict_raw.values())
-        datadict = form_dataset_from_strings(genome_strings, Alphabet_dir_blank)
 
     #pdb.set_trace()
     vcsmc = vcsmc.VCSMC(datadict,K=16)
 
-    vcsmc.train(numIters=1000, memory_optimization=args.memory_optimization)
+    vcsmc.train(epochs=100, batch_size=128, memory_optimization=args.memory_optimization)
